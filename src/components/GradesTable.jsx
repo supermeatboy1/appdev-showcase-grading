ee6149032254b0eb9d2380b552c964d4fba9db3f
supermeatboy1 <124642134+supermeatboy1@users.noreply.github.com>
2025-07-03 02:15:51 +0800

    Add average row for criteria tabulation

--- src/components/GradesTable.jsx
+++ src/components/GradesTable.jsx
@@ -1,4 +1,7 @@
 const GradesTable = ({ grades }) => {
+  const sumOfCriterion = (criterionName) => {
+    return Math.round(grades.reduce((accumulator, entry) => accumulator + entry.grade[criterionName], 0) / grades.length, 2);
+  }
   return (
     <div className="p-4 bg-gray-900 text-gray-100">
       <table className="min-w-full border border-gray-700 text-sm text-left bg-gray-800 rounded-lg overflow-hidden">
@@ -30,6 +33,16 @@ const GradesTable = ({ grades }) => {
                 <td className="px-4 py-2 border border-gray-700 text-center"></td>
                 <td className="px-4 py-2 border border-gray-700 text-center"></td>
                 <td className="px-4 py-2 border border-gray-700 text-center"></td>
+              </tr>
+          }
+          {
+            // A row for the average for each criterion
+            grades.length > 0 && 
+              <tr key={"sum"} className="bg-gray-500 border-b border-gray-700 h-8">
+                <td className="px-4 py-2 border border-gray-700 text-center">{sumOfCriterion("design")}</td>
+                <td className="px-4 py-2 border border-gray-700 text-center">{sumOfCriterion("usability")}</td>
+                <td className="px-4 py-2 border border-gray-700 text-center">{sumOfCriterion("functionality")}</td>
+                <td className="px-4 py-2 border border-gray-700 text-center">{sumOfCriterion("installation")}</td>
               </tr>
           }
         </tbody>
